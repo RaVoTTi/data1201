2bed8870e0cc7ccff1d97a238ed4331e1dced343
vravotti <vravotti@mtu.edu>
2024-10-31 17:17:01 -0400

    as

--- populate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "First Echo: $@"
-# It prints all the arguments
-echo "Second Echo: $#"
-# It prints the quantity of arguments received
-echo "Third Echo: $0"
-# It prints the file name
-echo "Fourth Echo: $1"
-# It prints the first argument
